58147f055def23fa298df2f340084926872ddc26
blueish <blueish4@users.noreply.github.com>
2020-01-08 15:12:57 +0000

    initial commit

---
+++ include/config.h
@@ -0,0 +1,23 @@
+#define WIFI_ENABLED 1
+#define LORA_ENABLED 1
+
+#ifndef DEBUG
+#define DEBUG 1
+#endif
+
+// Transmit every n seconds
+#define UPDATE_INTERVAL 60
+
+#define WIFI_SSID "ssid"
+#define WIFI_PSK "password"
+#define MQTT_SERVER
+#define MQTT_USERNAME
+#define MQTT_PASSWORD
+
+// Used in LORA ABP activation
+#define LORA_NETWORK_KEY
+#define LORA_APP_KEY
+#define LORA_DEV_ADDR
+
+// Enable only one channel (868.1MHz), or all channels
+#define LORA_SINGLE_CHANNEL 0
