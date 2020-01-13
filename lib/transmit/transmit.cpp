c21ec3b14b68a83e85e7f10527e2e1a1b206e86c
blueish <blueish4@users.noreply.github.com>
2020-01-13 18:15:17 +0000

    wifi connect to WPA2-PSK tested

--- lib/transmit/transmit.cpp
+++ lib/transmit/transmit.cpp
@@ -3,26 +3,36 @@
  * Library file to contain the transmission methods and helpers
  * Copyright (C) blueish 2020
  */
-#include <WiFiClientSecure.h>
-#include "esp_wpa2.h"
+
 #include "config.h"
+
+#include <WiFiClientSecure.h>
+#include <esp_wpa2.h>
 #include "debug.h"
 bool sendData(int* data) {
     #ifdef WIFI_ENABLED
 
     #endif
-
+    return true;
 }
 
 void init_send() {
-    #ifdef WIFI_ENABLED
+    #if WIFI_ENABLED
+    dbg_print("wifi support on");
     WiFi.disconnect(true);
     WiFi.mode(WIFI_STA);
+    #ifdef EAP_IDENTITY
+    //WPA2-Enterprise (eduroam)
     esp_wifi_sta_wpa2_ent_set_username((uint8_t *)EAP_IDENTITY, strlen(EAP_IDENTITY));
     esp_wifi_sta_wpa2_ent_set_password((uint8_t *)EAP_PASSWORD, strlen(EAP_PASSWORD));
     esp_wpa2_config_t config = WPA2_CONFIG_INIT_DEFAULT(); //set config settings to default
     esp_wifi_sta_wpa2_ent_enable(&config); //set config settings to enable function
     WiFi.begin(WIFI_SSID); //connect to wifi
+    #else
+    // WPA2-PSK
+    WiFi.begin(WIFI_SSID, WIFI_PSK);
+    #endif
+
     int counter = 0;
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
@@ -32,6 +42,6 @@ void init_send() {
             ESP.restart();
         }
     }
+    dbg_print("connected!");
     #endif
-    dbg_print("send ready");
 }
