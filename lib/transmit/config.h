16a90d476234043ae1987da802c23a32a2d0d3f7
Michael Hathaway <michael@hathaways.me.uk>
2020-01-13 18:15:17 +0000

    wifi connect to WPA2-PSK tested

---
+++ lib/transmit/config.h
@@ -0,0 +1,19 @@
+#define WIFI_ENABLED 1
+#define LORA_ENABLED 1
+
+#define WIFI_SSID ""
+// Use this for WPA2-PSK
+#define WIFI_PSK ""
+// Use this for WPA2-Enterprise (ie. eduroam)
+//#define EAP_IDENTITY
+//#define EAP_PASSWORD
+
+
+#define MQTT_SERVER
+#define MQTT_USERNAME
+#define MQTT_PASSWORD
+
+// Used in LORA ABP activation
+#define LORA_NETWORK_KEY
+#define LORA_APP_KEY
+#define LORA_DEV_ADDR
