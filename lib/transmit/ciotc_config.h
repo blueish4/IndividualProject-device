d02761198e9ee93e7ddab5392cebdb450eb5ad6d
Michael Hathaway <michael@hathaways.me.uk>
2020-01-20 21:24:30 +0000

    config with private key redacted

---
+++ lib/transmit/ciotc_config.h
@@ -0,0 +1,84 @@
+/******************************************************************************
+ * Copyright 2018 Google
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+// This file contains your configuration used to connect to Cloud IoT Core
+
+// Wifi network details.
+// Use this for WPA2-PSK
+//#define WIFI_PSK ""
+// Use this for WPA2-Enterprise (ie. eduroam)
+
+// Cloud iot details.
+const char *project_id = "individual-project-265621";
+const char *location = "europe-west1";
+const char *registry_id = "audio-monitors";
+const char *device_id = "node-1";
+
+// Configuration for NTP
+const char* ntp_primary = "pool.ntp.org";
+const char* ntp_secondary = "time.nist.gov";
+
+#ifndef LED_BUILTIN
+#define LED_BUILTIN 13
+#endif
+
+// To get the private key run (where private-key.pem is the ec private key
+// used to create the certificate uploaded to google cloud iot):
+// openssl ec -in <private-key.pem> -noout -text
+// and copy priv: part.
+// The key length should be exactly the same as the key length bellow (32 pairs
+// of hex digits). If it's bigger and it starts with "00:" delete the "00:". If
+// it's smaller add "00:" to the start. If it's too big or too small something
+// is probably wrong with your key.
+const char *private_key_str =
+    "";
+
+// Time (seconds) to expire token += 20 minutes for drift
+const int jwt_exp_secs = 3600; // Maximum 24H (3600*24)
+
+// To get the certificate for your region run:
+//   openssl s_client -showcerts -connect mqtt.googleapis.com:8883
+// for standard mqtt or for LTS:
+//   openssl s_client -showcerts -connect mqtt.2030.ltsapis.goog:8883
+// Copy the certificate (all lines between and including ---BEGIN CERTIFICATE---
+// and --END CERTIFICATE--) to root.cert and put here on the root_cert variable.
+
+const char *root_cert =
+    "-----BEGIN CERTIFICATE-----\n"
+    "MIIC0TCCAnagAwIBAgINAfQKmcm3qFVwT0+3nTAKBggqhkjOPQQDAjBEMQswCQYD\n"
+    "VQQGEwJVUzEiMCAGA1UEChMZR29vZ2xlIFRydXN0IFNlcnZpY2VzIExMQzERMA8G\n"
+    "A1UEAxMIR1RTIExUU1IwHhcNMTkwMTIzMDAwMDQyWhcNMjkwNDAxMDAwMDQyWjBE\n"
+    "MQswCQYDVQQGEwJVUzEiMCAGA1UEChMZR29vZ2xlIFRydXN0IFNlcnZpY2VzIExM\n"
+    "QzERMA8GA1UEAxMIR1RTIExUU1gwWTATBgcqhkjOPQIBBggqhkjOPQMBBwNCAARr\n"
+    "6/PTsGoOg9fXhJkj3CAk6C6DxHPnZ1I+ER40vEe290xgTp0gVplokojbN3pFx07f\n"
+    "zYGYAX5EK7gDQYuhpQGIo4IBSzCCAUcwDgYDVR0PAQH/BAQDAgGGMB0GA1UdJQQW\n"
+    "MBQGCCsGAQUFBwMBBggrBgEFBQcDAjASBgNVHRMBAf8ECDAGAQH/AgEAMB0GA1Ud\n"
+    "DgQWBBSzK6ugSBx+E4rJCMRAQiKiNlHiCjAfBgNVHSMEGDAWgBQ+/v/MUuu/ND49\n"
+    "80DQ5CWxX7i7UjBpBggrBgEFBQcBAQRdMFswKAYIKwYBBQUHMAGGHGh0dHA6Ly9v\n"
+    "Y3NwLnBraS5nb29nL2d0c2x0c3IwLwYIKwYBBQUHMAKGI2h0dHA6Ly9wa2kuZ29v\n"
+    "Zy9ndHNsdHNyL2d0c2x0c3IuY3J0MDgGA1UdHwQxMC8wLaAroCmGJ2h0dHA6Ly9j\n"
+    "cmwucGtpLmdvb2cvZ3RzbHRzci9ndHNsdHNyLmNybDAdBgNVHSAEFjAUMAgGBmeB\n"
+    "DAECATAIBgZngQwBAgIwCgYIKoZIzj0EAwIDSQAwRgIhAPWeg2v4yeimG+lzmZAC\n"
+    "DJOlalpsiwJR0VOeapY8/7aQAiEAiwRsSQXUmfVUW+N643GgvuMH70o2Agz8w67f\n"
+    "SX+k+Lc=\n"
+    "-----END CERTIFICATE-----";
+
+
+// In case we ever need extra topics
+const int ex_num_topics = 0;
+const char* ex_topics[ex_num_topics];
+//const int ex_num_topics = 1;
+//const char* ex_topics[ex_num_topics] = {
+//  "/devices/my-device/tbd/#"
+//};
