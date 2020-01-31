d42af471f6dce01b76931d334f655e521e15116f
blueish <blueish4@users.noreply.github.com>
2020-01-31 00:31:10 +0000

    now sends data over binary format

--- src/node.cpp
+++ src/node.cpp
@@ -3,13 +3,13 @@
  * Copyright (C) blueish 2020
  */
 #include <Arduino.h>
-#include <WiFi.h>
-
-#include "arduinoFFT.h"
+#include <string>
+#include <sstream>
 
 #include "transmit.h"
 #include "debug.h"
 #include "transform.h"
+#include "pack.h"
 #include "cfg.h"
 
 double boundaries[9] = {31.5, 63.0, 125.0, 250.0, 500.0, 1000.0, 2000.0, 4000.0, 8000.0};
@@ -19,7 +19,7 @@ unsigned long lastSample = 0;
 void sampleAudio(){
     // block until we have waited long enough
     // this may undersample, but can't oversample
-    while(micros()<lastSample + (1E6)/16500) {}
+    while(micros()<lastSample + (1E6)/40000) {}
     //For the moment, this is analog over pin 36
     lastSample = micros();
     insertValue(analogRead(36));
@@ -29,6 +29,8 @@ void setup(){
     init_dbg();
     init_send();
     init_transform(boundaries);
+    pinMode(36, INPUT);
+    dbg_print("Initialised!");
 }
 
 void loop(){
@@ -36,23 +38,26 @@ void loop(){
     while(true) {
         //sampleAudio();
         //Serial.println(vReal[currentSample]);
-        for (int i=0;i<2048;i++){
+        for (int i=0;i<1024;i++){
             sampleAudio();
         }
-        auto* data = generateFFT();
+        uint16_t bands[9];
+        const valuePack data = generateFFT(bands);
         send_loop();
 
         // publish a message roughly every second (+ sample and calculate time).
         // this will end up junking a lot of data if sample time << 1 second
-        if (millis() - lastMillis > 6000) {
+        if (millis() - lastMillis > UPDATE_INTERVAL*100) {
             lastMillis = millis();
-            std::string stringified = "";
+            void* sendBuffer = malloc(sizeof(double)+sizeof(uint16_t)*9);
+            memcpy(sendBuffer, &data.majorPeak, sizeof(data.majorPeak));
             for(int i=0;i<9;i++) {  //TODO: prune magic number here
-                stringified += to_string(data[i]) + " ";
-                data[i] = 0;
+                memcpy(sendBuffer+sizeof(double)+i*sizeof(uint16_t), &data.frequencies[i], sizeof(uint16_t));
+                data.frequencies[i] = 0;
             }
-            // stringify array, TODO make a binary transaction
-            sendData(String(stringified.c_str()));
+
+            sendData((const char*)sendBuffer, sizeof(double)+sizeof(uint16_t)*9);
+            free(sendBuffer);
         }
     }
 }
