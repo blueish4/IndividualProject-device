5a1bfc4aa92ad1c7873716d34e30b070099c2889
Michael Hathaway <michael@hathaways.me.uk>
2020-01-31 00:31:10 +0000

    now sends data over binary format

--- lib/transform/trans_objects.h
+++ lib/transform/trans_objects.h
@@ -1,11 +1,10 @@
 #ifndef TRANS_OBJECTS_H_
 #define TRANS_OBJECTS_H_
 
-#define samplingFrequency 16500
-#define FFT_SAMPLES 2048
+#define samplingFrequency 40000
+#define FFT_SAMPLES 1024
 
 int boundaryIndexes[9];
 double vReal[FFT_SAMPLES];
 double vImag[FFT_SAMPLES];
-uint16_t currentSample = 0;
 #endif
