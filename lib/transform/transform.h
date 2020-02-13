f6bf988c3f4cb8b83ee25fa43437fb699218297d
blueish <blueish4@users.noreply.github.com>
2020-02-13 16:10:37 +0000

    split the fft generation and binning to allow averaging

--- lib/transform/transform.h
+++ lib/transform/transform.h
@@ -1,7 +1,8 @@
 #include "pack.h"
-#define samplingFrequency 16500
-#define FFT_SAMPLES 4096
-valuePack generateFFT(uint16_t bands[8], double boundaries[8]);
+#define samplingFrequency 40000
+#define FFT_SAMPLES 2048
+uint16_t* generateFFT();
+valuePack binFFT(uint16_t bands[8], uint16_t* fft, double* boundaries);
 void init_transform(double* boundaries);
 void insertValue(int value);
 void insertValue(int value, int place);
